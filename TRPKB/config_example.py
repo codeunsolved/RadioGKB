c2d42b8c16d57bdd0d08b83cea1be1c33d2f998b
codeunsolved <codeunsolved@gmail.com>
2017-07-10 17:23:05 +0800

    17/07/10

---
+++ TRPKB/config_example.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# PROGRAM : config_example
+
+SECRET_KEY = ''
+
+# Database
+postgresql = {
+    'database': '',
+    'user': '',
+    'password': '',
+    'host': '',
+    'port': '',
+}
+
+# E-mail
+EMAIL_USE_SSL = True
+
+EMAIL_HOST = ''
+
+EMAIL_PORT = 25
+
+EMAIL_HOST_USER = ''
+
+EMAIL_HOST_PASSWORD = ''
+
+DEFAULT_FROM_EMAIL = ''
