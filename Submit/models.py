e9d02e253c443555025875a88d16019ffa7d5b4b
codeunsolved <codeunsolved@gmail.com>
2017-07-12 22:30:14 +0800

    17/07/12

--- Submit/models.py
+++ Submit/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import JSONField
 
-# Create your models here.
+
+class Draft(models.Model):
+    STATE_CHOICES = (
+    ('Draft', 'Draft'),
+    ('Under Review', 'Under Review'),
+    ('Revision', 'Revision'),
+    ('Accepted', ' Accepted'),
+    ('Rejected', 'Rejected'),)
+
+    KB_CHOICES = (
+    ('SNP', 'SNP'),
+    ('Expression', 'Expression'),)
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    state = models.CharField(max_length=20, choices=STATE_CHOICES)
+    kb = models.CharField(max_length=20, choices=KB_CHOICES)
+    pubmed_id = models.IntegerField()
+    content = JSONField()
+
+    def __str__(self):
+        return "{!s} | {} | {} | {}".format(self.user, self.state, self.kb, self.pubmed_id)
